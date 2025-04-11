6cd27644afda64e773c35a0f97444a3143d4e3ea
Tal Davidi <tal.davidi@gmail.com>
2025-04-10 21:32:43 -0700

    Add missing deps

--- google_calendar.py
+++ google_calendar.py
@@ -1,5 +1,5 @@
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 import os
 import datetime
